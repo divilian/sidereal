132131b25d717bf0fc6cd0b6fc90a1ff4f87a5e5
Stephen <stephen@umw.edu>
2022-11-24 18:02:36 -0500

    Add and use Converter class.

--- sim.py
+++ sim.py
@@ -1,2 +1,25 @@
 
+import sys
+import os.path
+import srcstuff
+import importlib
+importlib.reload(srcstuff)
+
 print("Sidereal Confluence simulator")
+
+usage_msg = "Usage: sim.py startingCardsFile.src."
+
+if len(sys.argv) != 2:
+    sys.exit(usage_msg)
+if not sys.argv[1].endswith(".src"): 
+    sys.exit(usage_msg)
+filename = sys.argv[1]
+
+if not os.path.exists(filename):
+    sys.exit(f"No such file {filename}.")
+
+with open(filename, encoding="utf-8") as f:
+    converters = [ srcstuff.Converter(l) for l in f.readlines() ]
+
+for c in converters:
+    print(c)
---
+++ srcstuff.py
@@ -0,0 +1,48 @@
+
+import re
+
+class Spec():
+    def __init__(self, color, amt):
+        self.color = color
+        self.amt = int(amt)
+    def __repr__(self):
+        return str(self.amt) + " " + \
+            (self.color if self.amt == 1 else self.color + "s")
+
+# Do inputs and outputs differ in anything important?
+#class Requirement(Spec):
+#    def __init__(self, color, amt):
+#        super().__init__(color,amt)
+#
+#class Product(Spec):
+#    def __init__(self, color, amt):
+#        super().__init__(color,amt)
+
+
+class Converter():
+    def __init__(self):
+        pass
+    def __init__(self, line):
+        self.reqs = set()
+        self.prods = set()
+        pat = re.compile(r"(?P<name>.*): \((?P<inp>.*)\) -> \((?P<outp>.*)\)")
+        file_info = pat.match(line.strip())
+        self.name = file_info['name']
+        self.reqs = self.process(file_info['inp'])
+        self.prods = self.process(file_info['outp'])
+
+    def process(self, line):
+        pattern = re.compile(r"(?P<color>[a-z]+)(?P<amt>\d+)")
+        parts = line.split(",")
+        ret_val = set()
+        for p in parts:
+            line_info = pattern.match(p)
+            ret_val |= {Spec(line_info['color'],line_info['amt'])}
+        return ret_val
+
+    def __repr__(self):
+        ret_val = self.name.title() + ": "
+        ret_val += ' and '.join([ r.__repr__() for r in self.reqs ]) + " -> "
+        ret_val += ' and '.join([ r.__repr__() for r in self.prods ])
+        return ret_val
+
